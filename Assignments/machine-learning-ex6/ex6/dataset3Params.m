57710c6da75561971fff426339d16254d45bc4ae
Arthur Zhang <arthurzh@umich.edu>
2019-08-12 15:50:46 -0700

    finished week 7 programming

--- Assignments/machine-learning-ex6/ex6/dataset3Params.m
+++ Assignments/machine-learning-ex6/ex6/dataset3Params.m
@@ -23,10 +23,23 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C_arr = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigma_arr = [0.01 0.03 0.1 0.3 1 3 10 30];
 
-
-
-
+% Compute errors matrix
+err_min = 1;
+for i = 1:size(C_arr, 2)
+    for j = 1:size(sigma_arr, 2)
+        model = svmTrain(X, y, C_arr(i), @(x1, x2) gaussianKernel(x1, x2, sigma_arr(j)));
+        predictions = svmPredict(model, Xval);
+        err = mean(double(predictions ~= yval));
+        if err < err_min
+            C = C_arr(i);
+            sigma = sigma_arr(j);
+            err_min = err;
+        end
+    end
+end
 
 
 % =========================================================================
